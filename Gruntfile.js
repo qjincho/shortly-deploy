4a98a716a91780a16ea01a46bf0904717b1781ce
Qjin Cho <qjin86@gmail.com>
2016-05-12 15:04:52 +0900

    deploy atThu May 12 2016 15:04:49 GMT+0900 (KST)

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,21 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      options: {
+        separator: ';',
+      },
+      library_concat: {
+        src: ['public/lib/jquery.js',
+          'public/lib/underscore.js',
+          'public/lib/backbone.js',
+          'public/lib/handlebars.js'
+        ],
+        dest: 'public/dist/lib.js',
+      },
+      client_concat: {
+        src: ['public/client/app.js', 'public/client/link.js', 'public/client/links.js', 'public/client/linkView.js', 'public/client/linksView.js', 'public/client/createLinkView.js', 'public/client/router.js'],
+        dest: 'public/dist/client.js',
+      },
     },
 
     mochaTest: {
@@ -21,16 +36,26 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      library: {
+        files: {
+          'public/dist/lib.min.js': ['public/dist/lib.js']
+        }
+      },
+      client: {
+        files: {
+          'public/dist/client.min.js': ['public/dist/client.js']
+        }
+      }
     },
 
     eslint: {
       target: [
         // Add list of files to lint here
+
       ]
     },
 
-    cssmin: {
-    },
+    cssmin: {},
 
     watch: {
       scripts: {
@@ -51,6 +76,17 @@ module.exports = function(grunt) {
 
     shell: {
       prodServer: {
+        command: [
+          'git add .',
+          'git commit -m \'deploy at' + new Date().toString() + '\'',
+          'git push live master'
+        ].join('&&'),
+        options: {
+          callback: function log(err, stdout, stderr, cb) {
+            console.log(stdout);
+            cb();
+          }
+        }
       }
     },
   });
@@ -64,7 +100,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
 
-  grunt.registerTask('server-dev', function (target) {
+  grunt.registerTask('server-dev', function(target) {
     // Running nodejs in a different process and displaying output on the main console
     var nodemon = grunt.util.spawn({
       cmd: 'grunt',
@@ -74,16 +110,9 @@ module.exports = function(grunt) {
     nodemon.stdout.pipe(process.stdout);
     nodemon.stderr.pipe(process.stderr);
 
-    grunt.task.run([ 'watch' ]);
+    grunt.task.run(['watch']);
   });
 
-
-  grunt.registerTask('upload', function(n) {
-    if (grunt.option('prod')) {
-      // add your production server task here
-    }
-    grunt.task.run([ 'server-dev' ]);
-  });
 
   ////////////////////////////////////////////////////
   // Main grunt tasks
@@ -93,19 +122,22 @@ module.exports = function(grunt) {
     'mochaTest'
   ]);
 
-  grunt.registerTask('build', [
-  ]);
+  grunt.registerTask('build', ['concat', 'uglify']);
 
   grunt.registerTask('upload', function(n) {
     if (grunt.option('prod')) {
       // add your production server task here
+      grunt.task.run(['deploy']);
     } else {
-      grunt.task.run([ 'server-dev' ]);
+      grunt.task.run(['server-dev']);
     }
   });
 
   grunt.registerTask('deploy', [
     // add your deploy tasks here
+    // 'test',
+    'build',
+    'shell'
   ]);
 
 
